3690bba132796284f2ee185e94a33bf2453e03ef
Ardillo <fakeMail@gmail.com>
2014-02-13 09:55:02 +0100

    Made some directory's

---
+++ Opdracht2/opdracht2.cpp
@@ -0,0 +1,30 @@
+/*
+ * naam: Jeroen van Prooijen
+ * stud.nr: 1595111
+ * 
+ * datum: 13-02-2014
+ * 
+ * Beschrijving:
+ * Practicum Opdracht 2 (coderen - decoderen) 
+ *
+ * Paragraaf 13.12 
+ * Opdracht 5 decoderen
+ * Opdracht 6 coderen
+ * 
+ * Voorwaarden:
+ * Maak gebruik van:
+ * 	stack
+ * 	deque
+ * 	memory
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <ConsoleColorLinux.h>
+
+using namespace std;
+
+int main() {
+
+   return 0;
+}
