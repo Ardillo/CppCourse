afb89873bc24985f583ec7ef73800c47c6e3e858
Ardillo <fakeMail@gmail.com>
2014-03-08 21:02:10 +0100

    Came far, need some getters and overloaded '==' operator for Date. That's for tommorrow

--- Opdracht4/test.cpp
+++ Opdracht4/test.cpp
@@ -17,95 +17,132 @@
 using namespace std;
 
 class Datum{
-   private:
-      
-      int dag, maand, jaar;
-      
+   private:     
+      int dag, maand, jaar;     
    public:
-
+      //constructor
       Datum(int dag, int maand, int jaar)
       :dag(dag), maand(maand), jaar(jaar){}
-	    
+      //functies	    
       friend ostream& operator<< (ostream &out, const Datum &date){
 	 return out << setw(2) << setfill('0') << date.dag << '-'
 			       << setw(2)      << date.maand << '-'
 			       << setw(4)      << date.jaar;
       }
+      ///TODO overloaden van '==' operator
 };
 
 class TentamenResultaat{
-   private:
-      
+   private:      
       string naam;
       double cijfer;
-      Datum datum;
-      
+      Datum datum;      
    public:
-      
+      //constructor
       TentamenResultaat(string naam, double cijfer, const Datum date)
       :naam(naam), cijfer(cijfer), datum(date){}
-      
+      //functies
       friend ostream& operator<< (ostream &out, const TentamenResultaat &Tres){
 	 return out << setfill(' ') << left
-	            << setw(8) << "Vak: " << Tres.naam << endl
-	            << setw(8) << "Datum: " << Tres.datum << endl
-	            << setw(8) << "Cijfer: " << Tres.cijfer << endl;
+	            << setw(8) << "\tVak: " << Tres.naam << endl
+	            << setw(8) << "\tDatum: " << Tres.datum << endl
+	            << setw(8) << "\tCijfer: " << Tres.cijfer << endl;
       }
 };
 
 class Student{
    private:
- 
       string voornaam, tussenvoegsel, achternaam;
       Datum gebDatum;
       vector<TentamenResultaat> resultaten;
-
    public:
-      
       // constructor met tussenvoegsel
       Student(string voornaam, string tussenvoegsel, string achternaam,
 	 const Datum gebDatum)
       :voornaam(voornaam), tussenvoegsel(tussenvoegsel),
        achternaam(achternaam), gebDatum(gebDatum){}
-       
       //constructor zonder tussenvoegsel
       Student(string voornaam, string achternaam, const Datum gebDatum)
       :voornaam(voornaam), tussenvoegsel(),
        achternaam(achternaam), gebDatum(gebDatum){}
-       
+      //functies 
       friend ostream& operator<<(ostream &out, const Student &stud);
-      void voegResultToe( const TentamenResultaat &result);
-      void printResultaten();
-             
+      void addResult( const TentamenResultaat &result);
+      void printResult();	///TODO
+      void getVoornaam();	///TODO
+      void getTussenvoegsel();	///TODO
+      void getAchternaam();	///TODO
+      void getGebDatum(); 	///TODO
+};
+
+class CijferAdmin{
+   private:
+      string naam;
+      vector<Student> studenten; // blz 296
+      
+   public:
+      
+      CijferAdmin(const string naam):naam(naam){};
+          
+      void voegStudentToe(const Student &student);
+      void voegCijferToe(const Student &student, const TentamenResultaat
+			 &result);      
+      void printStudent(const Student &student);
+      void printAdministratie();
 };
 
 int main(){
-   Datum gebDatumJoop(1, 1, 1970);   
-   Student st1("Joop", "Parkiet", gebDatumJoop);
-   cout << st1 << endl;
+   CijferAdmin administratie("cijfers");
    
-   TentamenResultaat result("wiskunde", 7.5, Datum(1,3,2014));
-   st1.voegResultToe(result);
+   Student st1("Joop", "Parkiet", Datum(1, 1, 1970));
+   Student st2("Kees", "de", "Beer", Datum(4,5,1983));
+   Student st3("Annemarie", "Bezema", Datum(8,9,1982));
+       
+   st1.addResult(TentamenResultaat("wiskunde", 7.5, Datum(1,3,2014)));   
+   st1.addResult(TentamenResultaat("biologie", 6, Datum(3,3,2014)));
    
-   st1.voegResultToe(TentamenResultaat("biologie", 6, Datum(3,3,2014)));
+   st2.addResult(TentamenResultaat("techniek", 8.3, Datum(2,3,2014)));
    
-   st1.printResultaten();
-   cout << st1 << endl;
+   st3.addResult(TentamenResultaat("frans", 4, Datum(2,1,2014)));
+   st3.addResult(TentamenResultaat("engels", 6, Datum(7,7,2013)));
+   st3.addResult(TentamenResultaat("biologie", 9, Datum(3,3,2014)));
+  
+   //cout << st2 << endl;
+   
+   administratie.voegStudentToe(st1);
+   administratie.voegStudentToe(st2);
+   administratie.voegStudentToe(st3);
+   
+   administratie.printStudent(st3);
+}
+//functies uit cijferAdmin class
+void CijferAdmin::voegStudentToe(const Student &student){
+   studenten.push_back(student);
+}
+
+void CijferAdmin::printStudent(const Student &student){
+   vector<Student>::const_iterator pos, einde=studenten.end();
+   for(pos = studenten.begin(); pos != einde; ++pos){
+      ///TODO getters and setters.voor student.
+      cout << *pos << endl;
+   }  
 }
 
 //functies uit student class
 
-void Student::voegResultToe( const TentamenResultaat &result){
+void Student::addResult( const TentamenResultaat &result){
    resultaten.push_back(result);   
 }
 
-void Student::printResultaten(){
-   vector<TentamenResultaat>::iterator pos, einde=resultaten.end();
+void Student::printResult(){
+   vector<TentamenResultaat>::const_iterator pos, einde=resultaten.end();
    for(pos = resultaten.begin(); pos != einde; ++pos){
       cout << *pos << endl;
    }   
 }
 
+// vraag: Indien een object const is, zijn dan alle onderliggende objecten ook
+//        const?
 ostream& operator<< (ostream &out, const Student &stud){
    
    out << setw(12) << setfill(' ') << left 
@@ -115,24 +152,24 @@ ostream& operator<< (ostream &out, const Student &stud){
    }
    out << ' ' << stud.achternaam << endl 
        << "Geb. datum: " << stud.gebDatum << endl << endl;
+   
+   // als er resultaten zijn dan lijst doorlopen.    
+   if (!stud.resultaten.empty()){
+      for(int i = 0; i<stud.resultaten.capacity(); ++i)
+	 out << stud.resultaten.at(i) << endl;
+   }
        
    return out;
 }
 
-/*
- * 
- * 
- * 
- * 
- * 
- * 
- *    //if (!stud.resultaten.empty()){
-      vector<TentamenResultaat>::const_iterator pos,einde=stud.resultaten.end();
-      cout << *pos << " -- " << *einde << endl;
-      
-      for(pos = stud.resultaten.begin(); pos != einde-1; ++pos){
-	 cout << "std::cout " << *pos << endl;
-	 out << *pos << endl << endl;	 
-      }
-   //}
+/* 
+ * Dit lijkt niet te werken --> Segmentation fault 
+ 
+   if (!stud.resultaten.empty()){
+     vector<TentamenResultaat>::const_iterator pos,einde=stud.resultaten.end();
+     for(pos = stud.resultaten.begin(); pos != einde; ++pos){
+	 cout << *pos << endl;
+     }
+   }
+   
  */
